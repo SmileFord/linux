d2bdbbb228480233033b88d118bb8600047655c3
evtest-hash <84323112+evtest-hash@users.noreply.github.com>
2026-01-16 10:41:43 +0800

    drm/rockchip: Add support for configurable HPD trigger level
    This patch adds support for a configurable HPD (Hot Plug Detect) trigger level
    in the Rockchip HDMI driver. The HPD trigger level can now be specified via
    the device tree property "hpd-trigger-level". If the property is not specified,
    the default value is set to high (1).

--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -257,6 +257,7 @@ struct rockchip_hdmi {
 	bool timing_force_output;
 	struct drm_display_mode force_mode;
 	u32 force_bus_format;
+	u32 hpd_trigger_level;
 };
 
 #define to_rockchip_hdmi(x)	container_of(x, struct rockchip_hdmi, x)
@@ -1565,6 +1566,11 @@ static int rockchip_hdmi_parse_dt(struct rockchip_hdmi *hdmi)
 		kfree(phy_config);
 	} else {
 		dev_dbg(hdmi->dev, "use default hdmi phy table\n");
+	}
+
+	if (of_property_read_u32(np, "hpd-trigger-level", &hdmi->hpd_trigger_level)) {
+		hdmi->hpd_trigger_level = 1;
+		dev_warn(hdmi->dev, "failed to get hpd-trigger-level, set high as default\n");
 	}
 
 	if (of_property_read_bool(np, "force-output")) {
@@ -3289,6 +3295,10 @@ dw_hdmi_rk3588_read_hpd(struct dw_hdmi_qp *dw_hdmi, void *data)
 	struct rockchip_hdmi *hdmi = (struct rockchip_hdmi *)data;
 
 	regmap_read(hdmi->regmap, RK3588_GRF_SOC_STATUS1, &val);
+
+	if (hdmi->hpd_trigger_level == 0) {
+		val = ~val;
+	}
 
 	if (!hdmi->id) {
 		if (val & RK3588_HDMI0_LEVEL_INT) {
